c879be984f6744ae51f821940c13102519913879
kk1112k <kk1112k@naver.com>
2021-10-31 20:35:08 +0900

    안ㄴ녕

---
+++ src/main/java/com/example/heeso/HeesoApplication.java
@@ -0,0 +1,13 @@
+package com.example.heeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeesoApplication.class, args);
+	}
+
+}
